f1a91bfb255e17f4523c75c66a85637efb8aaea5
Zac Connelly <zac@counsyl.com>
2015-12-16 17:57:52 -0800

    add pingme to install script and reorder some stuff / add comments

--- install.sh
+++ install.sh
@@ -5,13 +5,20 @@ for dot in $(ls -A); do
 		ln -sf "$PWD/$dot" "$target"
 	fi
 done
+
+# uncomment below to include pingme (must be on vpn)
+# git clone git@github.counsyl.com:zac/ping_me.git ~/ &> /dev/null
+
+# Install and configure Vundle
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim &> /dev/null
 vim +PluginInstall +qall
+
+# Global Git Config
+git config --global core.excludesfile '~/.gitignore'
+
 # Uncomment the appropriate lines
 
 # git config --global user.email "zconnelly13@gmail.com"
 # git config --global user.name "Zac Connelly"
-
 git config --global user.email "zac@counsyl.com"
 git config --global user.name "Zac Connelly"
-git config --global core.excludesfile '~/.gitignore'
